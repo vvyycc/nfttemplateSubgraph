36767447407d77a4d2a88f0014e5595ef63d40f2
Victor <victorroldan18@gmail.com>
2023-06-28 13:33:54 +0200

    Added subgraph

---
+++ tests/nft-factory-utils.ts
@@ -0,0 +1,142 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchMetadataUpdate,
+  CreatedNFT,
+  MetadataUpdate,
+  Transfer
+} from "../generated/nftFactory/nftFactory"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createBatchMetadataUpdateEvent(
+  _fromTokenId: BigInt,
+  _toTokenId: BigInt
+): BatchMetadataUpdate {
+  let batchMetadataUpdateEvent = changetype<BatchMetadataUpdate>(newMockEvent())
+
+  batchMetadataUpdateEvent.parameters = new Array()
+
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_fromTokenId",
+      ethereum.Value.fromUnsignedBigInt(_fromTokenId)
+    )
+  )
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_toTokenId",
+      ethereum.Value.fromUnsignedBigInt(_toTokenId)
+    )
+  )
+
+  return batchMetadataUpdateEvent
+}
+
+export function createCreatedNFTEvent(
+  tokenId: BigInt,
+  tokenURI: string
+): CreatedNFT {
+  let createdNftEvent = changetype<CreatedNFT>(newMockEvent())
+
+  createdNftEvent.parameters = new Array()
+
+  createdNftEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  createdNftEvent.parameters.push(
+    new ethereum.EventParam("tokenURI", ethereum.Value.fromString(tokenURI))
+  )
+
+  return createdNftEvent
+}
+
+export function createMetadataUpdateEvent(_tokenId: BigInt): MetadataUpdate {
+  let metadataUpdateEvent = changetype<MetadataUpdate>(newMockEvent())
+
+  metadataUpdateEvent.parameters = new Array()
+
+  metadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_tokenId",
+      ethereum.Value.fromUnsignedBigInt(_tokenId)
+    )
+  )
+
+  return metadataUpdateEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
